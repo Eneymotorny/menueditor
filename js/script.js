5378e3a32c49ad7b273ff671fa58754e58946795
Eneymotorny <eneymotorny@gmail.com>
2017-07-08 03:32:14 +0300

    add items, subitems

--- js/script.js
+++ js/script.js
@@ -3,69 +3,63 @@
 $(function() {
 
 	var
-		$tools = $('#menuPanelTool'),
+		$panelTools = $('#menuPanelTool'),
+		$tools = $panelTools.children('div'),
 		$main = $('#menuContainer');
 
-	var menus = {}, currentMenu = null;
+	var menus = {}, currentMenu = null, nestedLevel = 3;
 
 	console.log('all', menus);
 
+
 	$('#addMenu').on('click', function () {
 		var menuID = 'id' + (new Date().getTime());
-		var menu = menus[menuID] = setDefaultMenu();
+		menus[menuID] = setDefaultMenu();
 
 		console.log('build new', menus[menuID]);
 		var $menu = $('<div id="' + menuID + '">').addClass().appendTo($main);
 
-		buildMenu(menu, $menu);
+		buildMenu(menus[menuID], $menu);
 
 		$menu.on('dblclick', function () {
 				$main.find('.selected').removeClass('selected');
-				$menu.addClass('selected').children('ul').menu();
+				$menu.addClass('selected')/*.children('ul').menu()*/;
 				$tools.html('');
-				$tools.slideDown();
 				currentMenu = menuID;
-				buildTree(menu, $tools);
-
-				$tools.find('input').on('change', function () {
-					var $menu = $('#' + currentMenu);
-					readTree(currentMenu);
-					$menu.html('');
-					buildMenu(menus[currentMenu], $menu);
-					$menu.children('ul').menu();
-					console.log('rebuild', menus[currentMenu]);
-				});
-
+				buildTree(menus[menuID], $tools);
+				$panelTools.slideDown();
+				$tools.find('input').on('change', function () { rebuildAllBind() });
 		});
-
+		$menu.children('ul').menu()
 	});
-	$('#rebuildMenu').on('click', function () {
+	$('#rebuildMenu').on('click', function () { rebuildAllBind() });
+	var $btnAdd = $('#addItem').on('click', function () {
+		if ($tools.children('ul').length) {
+			var $ul = $tools.children('ul');
+		} else {
+			$ul = $('<ul>').appendTo($tools)
+		}
+		var $li = $('<li><input type="text"></li>').appendTo($ul);
+		addBtns($li)
+	});
+	$('<span>').addClass('ui-icon ui-icon-plusthick').appendTo($btnAdd);
+
+	function rebuildAllBind() {
 		var $menu = $('#' + currentMenu);
 		readTree(currentMenu);
 		$menu.html('');
 		buildMenu(menus[currentMenu], $menu);
 		$menu.children('ul').menu();
-		console.log('rebuild', menus[currentMenu]);
-	});
-	$tools.find('input').on('change', function () {
-		var $menu = $('#' + currentMenu);
-		readTree(currentMenu);
-		$menu.html('');
-		buildMenu(menus[currentMenu], $menu);
-		$menu.children('ul').menu();
-		console.log('rebuild', menus[currentMenu]);
-	});
-
-
-
+		$tools.find('input').on('change', function () { rebuildAllBind() });
+		/*console.log('rebuild', menus[currentMenu]);*/
+	}
 	function readTree(dataID) {
 		var menu = menus[dataID] = [];
-		console.log('read', menu);
+		/*console.log('read', menu);*/
 		readTreeIter($tools.children(), menu);
 
 		function readTreeIter(container, data) {
 			container.each(function (indx, domElem) {
-
 				var obj = data[indx] = {},
 					tag = $(domElem)[0].localName,
 					content = $(domElem).children('input').val();
@@ -75,7 +69,6 @@ $(function() {
 				if (content) {
 					obj.content = content
 				}
-
 				if ($(domElem).children('ul, li').length) {
 					var nested = obj.nested = [];
 					readTreeIter($(domElem).children('ul, li'), nested)
@@ -91,6 +84,7 @@ $(function() {
 				var $tag = $('<' + node.tag + '>').appendTo(target);
 				if (node.content) {
 					$tag.append('<input type="text" value="' + node.content + '">');
+					addBtns($tag);
 				}
 			}
 			if (node.nested && node.nested.length) {
@@ -158,5 +152,29 @@ $(function() {
 				content: 'item 3'
 			}]
 		}];
+	}
+	function addBtns(target) {
+		if (target.parents('ul').length < nestedLevel) {
+			var $btnNest = $('<button>').appendTo(target)
+				.on('click', function () {
+					if (target.children('ul').length) {
+						var $ul = target.children('ul');
+					} else {
+						$ul = $('<ul>').appendTo(target)
+					}
+					rebuildAllBind();
+					var $li = $('<li></li>').appendTo($ul);
+					$('<input type="text">').appendTo($li)
+						.on('change', function () { rebuildAllBind() });
+					addBtns($li)
+				});
+			$('<span>').addClass('ui-icon ui-icon-arrowreturnthick-1-e').appendTo($btnNest)
+		}
+		var $btnRemove = $('<button>').appendTo(target).on('click', function () {
+			target.remove();
+			rebuildAllBind()
+		});
+		$('<span>').addClass('ui-icon ui-icon-close').appendTo($btnRemove);
+		rebuildAllBind()
 	}
 });
